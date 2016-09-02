66911957d23e76a72a64d82e0dccd7642f6f7b20
garthdb <garthdb@gmail.com>
2016-09-02 14:15:25 -0600

    WIP getting resin updated

--- test/button.test.js
+++ test/button.test.js
@@ -1,32 +1,17 @@
-/**
- *
- * Copyright 2012 Adobe Systems Inc.;
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+var assert = require('assert');
+var fs = require('fs');
 
-/*global require, describe, it*/
+function read(file) {
+    return fs.readFileSync(file, 'utf8');
+}
 
-var grunt = require('grunt'),
-    assert = require('assert');
-
-describe('button-base', function() {
+describe('Topcoat Button Base', function() {
     'use strict';
 
     it('should create expected css file', function() {
-        var actual = grunt.file.read('css/button.css').trim();
-        var expected = grunt.file.read('test/expected/button.css').trim();
+        console.log(require('../package.json').name)
+        var actual = read('css/index.css').trim();
+        var expected = read('test/expected/index.css').trim();
         assert.equal(actual, expected, 'should generate correct css');
     });
 
