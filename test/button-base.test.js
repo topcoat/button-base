b2ecd0d257db72df87701e1d2caaf4e162ce66a5
garthdb <garthdb@gmail.com>
2016-09-07 10:07:36 -0600

    fix(Configuration): fixed broken path to style file

--- test/button-base.test.js
+++ test/button-base.test.js
@@ -2,14 +2,16 @@ import test from 'ava';
 import resin from 'resin';
 import fs from 'fs';
 
+
 test('compilation', t => {
   const input = './button-base.fixture.css';
   const expected = fs.readFileSync('./button-base.expected.css','utf-8').trim();
   return resin({
     src: input,
+    css: fs.readFileSync(input, 'utf-8'),
     vars: true,
     extend: true,
-    prepend: '../src/button-base.css'
+    prepend: '..'
   }).then(result => {
     t.is(result.css.trim(), expected);
   });
